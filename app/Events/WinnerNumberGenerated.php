f26df9889617c910afb93a33d8e88a4d0c1e02be
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-04-25 14:08:53 -0400

    ✨ add new event in proyect

---
+++ app/Events/WinnerNumberGenerated.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Support\Facades\Log;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class WinnerNumberGenerated
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+
+     public $number;
+    public function __construct($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+       
+Log::debug($this->number);
+Log::debug($this->time);
+return new Channel('game');
+    }
+}
