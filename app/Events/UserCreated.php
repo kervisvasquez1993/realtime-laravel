e214646b8f4dfe1a696a7fdfb4f42dfc0e22bbe3
kervisvasquez1993 <kervisvasquez24@gmail.com>
2021-04-24 21:35:18 -0400

    ✨ proyecto2 finalizamos el proyecto agregamos evento al js para renderizar en pantalla

--- app/Events/UserCreated.php
+++ app/Events/UserCreated.php
@@ -36,7 +36,7 @@ class UserCreated implements ShouldBroadcast
     public function broadcastOn()
     {
         
-        Log::debug("User created {$this->user->name}");
+        
         return new Channel('users');
     }
 }
