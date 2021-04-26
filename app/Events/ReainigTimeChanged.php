abc61ddfaa2805431993237e695d74fbe41126d3
kervis vasquez <kervisvasquez24@gmail.com>
2021-04-26 16:22:21 -0400

    🐛 proyecto3 solution of realtime in event

--- app/Events/ReainigTimeChanged.php
+++ app/Events/ReainigTimeChanged.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
-class ReainigTimeChanged
+class ReainigTimeChanged implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
